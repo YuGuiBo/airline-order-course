b524a45cb88bd3f72097378bed516b6deaff6b39
YuGuiBo <gui.bo.yu@ibm.com>
2025-10-30 08:53:04 +0800

    公网IP修改

--- backend/src/main/java/com/postion/airlineorderbackend/config/SwaggerConfig.java
+++ backend/src/main/java/com/postion/airlineorderbackend/config/SwaggerConfig.java
@@ -26,7 +26,7 @@ public class SwaggerConfig {
                         .url("https://github.com/tianyf925"))
                 )
                 .servers(java.util.Arrays.asList(
-                        new Server().url("http://16.170.254.50:8080"),
+                        new Server().url("http://13.51.162.54:8080"),
                         new Server().url("http://api.airline.com")
                 ))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
